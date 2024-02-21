e6afeab0a24cfc68bff392f1d447a3e20d8ecfec
INT019 <amasha@aadhira.lk>
2024-02-21 11:42:49 +0530

    Commit 01
    To test merging.

--- amarar/src/App.js
+++ amarar/src/App.js
@@ -4,20 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import pages
-import UserDashObituary from './pages/UserDashObituaryHome';
-import CreateObituary from './pages/CreateObituary';
-import DescriptionObituary from './pages/DescriptionObituary';
-import EditObituary from './pages/EditObituary';
+import ObituaryPriceLists from './pages/ObituaryPriceLists';
+import RemembrancePriceLists from './pages/RemembrancePriceLists';
 
 function App ()
 {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <UserDashObituary /> } />
-        <Route path='/create' element={ <CreateObituary /> } />
-        <Route path='/read/:id' element={ <DescriptionObituary /> } />
-        <Route path='/edit/:id' element={ <EditObituary /> } />
+        <Route path='/' element={ <ObituaryPriceLists /> } />
+        <Route path='/remembrance-price-list' element={ <RemembrancePriceLists /> } />
       </Routes>
     </BrowserRouter>
   );
