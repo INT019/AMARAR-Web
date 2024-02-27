b9d10e6fcf6d23e5abbbbfcc829bcaf9edf98db8
INT019 <amasha@aadhira.lk>
2024-02-27 16:47:39 +0530

    Commit
    Update some

--- amarar/src/App.js
+++ amarar/src/App.js
@@ -3,24 +3,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import pages
-import CreateRemembrance from './pages/CreateRemembrance';
-=======
-
-// obituary pages
-import CreateObituary from './pages/obituary/CreateObituary';
-import DescriptionObituary from './pages/obituary/DescriptionObituary';
-import EditObituary from './pages/obituary/EditObituary';
-import ObituaryNav from './components/ObituaryNav';
-import PhotoObituary from './pages/obituary/PhotoObituary';
-import TributeObituary from './pages/obituary/TributeObituary';
-import DonationObituary from './pages/obituary/DonationObituary';
-import UserDashObituaryHome from './pages/obituary/UserDashObituaryHome';
-
-// remembrance pages
-import UserRemembranceHome from './pages/remembrance/UserRemembranceHome';
-
 // payment pages
+import ObituaryPriceLists from './pages/ObituaryPriceLists';
+import RemembrancePriceLists from './pages/RemembrancePriceLists';
 
 function App ()
 {
@@ -30,12 +15,6 @@ function App ()
         {/* Payment Function */ }
         <Route path='/' element={ <ObituaryPriceLists /> } />
         <Route path='/remembrance-price-list' element={ <RemembrancePriceLists /> } />
-
-        {/* Remembrance Function*/ }
-
-        {/* Remembrance Function*/ }
-        <Route path='/remembrance-dashboard' element={ <UserRemembranceHome /> } />
-        <Route path='/create-remembrance' element={ <CreateRemembrance /> } />
       </Routes>
     </BrowserRouter>
   );
