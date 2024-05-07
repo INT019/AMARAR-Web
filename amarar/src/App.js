330fc5bec8b618282676eb491f58aa2eaab5efb6
Kavishka Bingun Sathsarani <108647821+KavishkaBingun@users.noreply.github.com>
2024-05-07 14:35:18 +0530

    Update App.js

--- amarar/src/App.js
+++ amarar/src/App.js
@@ -3,15 +3,33 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import pages
-import UserDashObituary from './pages/UserDashObituaryHome';
+
+//Donation User
+import AddDonation from "./pages/AddDonation";
+import DonationUser from "./pages/DonationsUser";
+import ObituaryPostPage from './pages/ObituaryPostPage';
+import DonationUserDash from './pages/UserDashDonation';
+
+//Donation Admin
+import AdminDashDonation from './pages/AminDashDonation';
+import FetchDonation from './pages/FetchDonation';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
 
 function App ()
 {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <UserDashObituary /> } />
+    <Route path="/donations" element={<AddDonation/>}/>
+    <Route path="/viewdonation" element={<DonationUser/>}/>
+    <Route path="/read/:id" element={<ObituaryPostPage/>}/>
+    <Route path="/donationUserDash" element={<DonationUserDash/>}/>
+
+    <Route path="/donationAdminDash" element={<AdminDashDonation/>}/>
+    <Route path="/donation/:id" element={<FetchDonation/>}/>
+    <Route path="/adminlogin" element={<AdminLogin/>}/>
+    <Route path="/admindashboard" element={<AdminDashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
